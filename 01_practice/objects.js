a0d895983281e5742233c841daeae48b75284a9e
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-01-09 16:22:32 +0530

    completed objects learning

--- 01_practice/objects.js
+++ 01_practice/objects.js
@@ -1,1 +1,15 @@
-console.log("hello World");
+const regularUser={
+    name:"John",
+    age:30, 
+    occupation:"Software Engineer",
+    hobbies:["reading","swimming","coding"],
+}
+
+console.log(Object.keys(regularUser));
+console.log(Object.values(regularUser));
+console.log(Object.entries(regularUser));
+
+
+console.log(regularUser.hasOwnProperty("age"));
+
+
