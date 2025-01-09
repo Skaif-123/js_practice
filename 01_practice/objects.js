f12d6eb39f379e9fd25cbecbc362c5fa26fd217a
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-01-09 16:33:29 +0530

    learn object destructuring

--- 01_practice/objects.js
+++ 01_practice/objects.js
@@ -5,11 +5,21 @@ const regularUser={
     hobbies:["reading","swimming","coding"],
 }
 
-console.log(Object.keys(regularUser));
-console.log(Object.values(regularUser));
-console.log(Object.entries(regularUser));
+// console.log(Object.keys(regularUser));
+// console.log(Object.values(regularUser));
+// console.log(Object.entries(regularUser));
 
 
-console.log(regularUser.hasOwnProperty("age"));
+// console.log(regularUser.hasOwnProperty("age"));
+
+
+// Destructuring  Objects
+const course={
+    coursename:"js in hindi",
+    price:"1900",
+    courseInstructor:"hitesh"
+}
+const { coursename,price,courseInstructor}=course;
+console.log(price);
 
 
