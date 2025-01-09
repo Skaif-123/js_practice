9fa331b37e95b1ddf25759b6ffb01d27d2f9d8a4
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-01-09 23:21:15 +0530

    learn functions

--- 01_practice/function.js
+++ 01_practice/function.js
@@ -1,5 +1,10 @@
-function userName(username){
-
-    return `${username} just logged in, Thank you`
+const user={
+    firstName:"Kaif",
+    lastName:"Bhombal",
+    age:25
 }
 
+function handleObject(anyobject){
+    console.log(`Username  intital is ${anyobject.firstName} and last name is ${anyobject.lastName} and age is ${anyobject.age}`);
+}
+handleObject(user)
